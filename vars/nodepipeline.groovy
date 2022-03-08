8cd4ba1f7b9ba3eb47ba42361a7f615957877bfd
Pásztor Bence <36498748+PasztorBence@users.noreply.github.com>
2022-03-08 13:29:10 +0100

    Update nodepipeline.groovy

--- vars/nodepipeline.groovy
+++ vars/nodepipeline.groovy
@@ -1,4 +1,9 @@
 def call(body){
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  def text = config.text
   node('vbox-slave'){
         stage('Repository Clone'){
            git branch: 'main', credentialsId: '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', url: 'https://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@github.com/PasztorBence/calendarclient.git'
@@ -6,6 +11,7 @@ def call(body){
         stage('Build'){
             configFileProvider([configFile(fileId: '2f0b42a7-b4e0-442c-b0b2-09e6792a3ac0', targetLocation: '.npmrc')]) {}
             sh 'npm install --registry=https://registry.npmjs.org/'
+          echo "${text}"
         }
         stage('Test'){
             sh 'npm test'
