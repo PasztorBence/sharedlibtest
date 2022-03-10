ea896236fbdceb65a74adbfa9597f64495ad1849
Pásztor Bence <peace_96@hotmail.hu>
2022-03-10 03:06:12 +0100

    Update mavenpipeline.groovy

--- vars/mavenpipeline.groovy
+++ vars/mavenpipeline.groovy
@@ -10,7 +10,7 @@ def call(body) {
         stage('Build/Publish') {
             def mavenPom = readMavenPom file: 'pom.xml'
             withMaven(jdk: 'jdk8', mavenSettingsConfig: '8cc2cb63-74a8-4de8-937e-938ca4b32dc9') {
-                if(isSnapshot){
+                if(isSnapshot == true){
                     mavenPom.version = "${mavenPom.version}-SNAPSHOT"
                     sh "mvn clean deploy"
                 } else {
