a6a43d2febbeaac88fc22a96e084d0847d9a35ad
Pásztor Bence <peace_96@hotmail.hu>
2022-03-10 02:54:22 +0100

    Update mavenpipeline.groovy

--- vars/mavenpipeline.groovy
+++ vars/mavenpipeline.groovy
@@ -10,7 +10,13 @@ def call(body) {
         stage('Build/Publish') {
             def mavenPom = readMavenPom file: 'pom.xml'
             withMaven(jdk: 'jdk8', mavenSettingsConfig: '8cc2cb63-74a8-4de8-937e-938ca4b32dc9') {
-                sh "mvn clean deploy -Dversion=${mavenPom.version}"
+                if(${isSnapshot} == true){
+                    sh "mvn versions:set -Dversion=${mavenPom.version}-SNAPSHOT"
+                    sh "mvn clean deploy"
+                } else {
+                    sh "mvn versions:set -Dversion=${mavenPom.version}-SNAPSHOT"
+                    sh "mvn clean deploy" 
+                }
             }
         }
         stage('Docker image Build') {
