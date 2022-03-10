eafa293a1ebf2264b00b1e5079d5beccae50ad71
Pásztor Bence <peace_96@hotmail.hu>
2022-03-10 01:37:03 +0100

    .

---
+++ vars/mavenpipeline.groovy
@@ -0,0 +1,22 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    node('vbox-slave') {
+        stage('Repository Clone') {
+            git branch: 'main', url: 'https://github.com/PasztorBence/mavenpipelinetest.git'
+        }
+        stage('Build/Publish') {
+            withMaven(jdk: 'jdk8', mavenSettingsConfig: '8cc2cb63-74a8-4de8-937e-938ca4b32dc9') {
+                sh 'mvn clean deploy'
+            }
+        }
+        stage('Docker image Build') {
+            dockerbuild('mavenpipelinetest')
+        }
+        stage('Docker Publish') {
+            sh 'docker push 192.168.0.105:8085/mavenpipelinetest:latest'
+        }
+    }
+}
--- vars/nodepipeline.groovy
+++ vars/nodepipeline.groovy
@@ -1,28 +1,26 @@
-def call(body){
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
-  def text = config.text
-  node('vbox-slave'){
-        stage('Repository Clone'){
-           git branch: 'main', credentialsId: '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', url: 'https://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@github.com/PasztorBence/calendarclient.git'
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    node('vbox-slave') {
+        stage('Repository Clone') {
+            git branch: 'main', credentialsId: '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', url: 'https://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@github.com/PasztorBence/calendarclient.git'
         }
-        stage('Build'){
-            configFileProvider([configFile(fileId: '2f0b42a7-b4e0-442c-b0b2-09e6792a3ac0', targetLocation: '.npmrc')]) {}
+        stage('Build') {
+            configFileProvider([configFile(fileId: '2f0b42a7-b4e0-442c-b0b2-09e6792a3ac0', targetLocation: '.npmrc')]) { }
             sh 'npm install --registry=https://registry.npmjs.org/'
-            echo "${text}"
         }
-        stage('Test'){
+        stage('Test') {
             sh 'npm test'
         }
-        stage('Publish'){
+        stage('Publish') {
             sh 'npm publish'
         }
-        stage('Docker image Build'){
+        stage('Docker image Build') {
             dockerbuild('calendarclient')
         }
-        stage('Docker Publish'){
+        stage('Docker Publish') {
             sh 'docker push 192.168.0.105:8085/calendarclientdocker:latest'
         }
     }
